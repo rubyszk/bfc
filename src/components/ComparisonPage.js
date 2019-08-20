9555f2c134d248ae7e7ceb51820e0ab09c6d3bc7
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-08-20 16:50:22 -0400

    done for the day

--- src/components/ComparisonPage.js
+++ src/components/ComparisonPage.js
@@ -2,15 +2,14 @@ import React, {Component} from 'react'
 
 class ComparisonPage extends Component {
     render() {
-        console.log(this.props)
         return (
             <div className='images'>
                 <div className='left'>
-                    <img src='https://cdn.pixabay.com/photo/2015/09/09/20/23/baby-933097__340.jpg'></img>
+                    <img src={this.props.babies.image}></img>
                 </div>
         
                 <div className='right'>
-                    <img src='https://cdn.pixabay.com/photo/2015/09/09/20/23/baby-933097__340.jpg'></img>
+                    <img src={this.props.babies.image}></img>
                 </div>
                 <a href='/babies/all'> Show All Fighters </a>
             </div>
