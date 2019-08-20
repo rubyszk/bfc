4c8adc8ef0ac81c3924f5ca66295eb70c5860dd5
Ruby King <ruby.sz.king@gmail.com>
2019-08-20 13:44:23 -0400

    index start

---
+++ src/components/Index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import React from 'react';
+
+let baseURL = 'http://localhost:3003'
+
+class Index extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+
+    }
+}
+
+export default Index;
