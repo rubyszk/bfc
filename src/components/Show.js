088d8e1a763588163e78079da833838cfc3353d6
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-20 15:15:44 -0400

    made Show component

--- src/components/Show.js
+++ src/components/Show.js
@@ -3,8 +3,14 @@ import React from 'react';
 class Show extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Show</h1>
+      <div className='show'>
+        <img scr={this.props.babies.image} />
+        <h1>{this.props.babies.name}</h1>
+        <h3>Age: {this.props.babies.age}</h3>
+        <h3>Weight: {this.props.babies.weight}</h3>
+        <h3>Wins: {this.props.babies.wins}</h3>
+        <h3>Losses: {this.props.babies.losses}</h3>
+        <h3>Bio: {this.props.babies.about}</h3>
       </div>
     );
   }
