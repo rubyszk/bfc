65a080ed194b43db17602c6862bf25fb368c6dcf
jmccasusi <jmccasusi@gmail.com>
2019-08-22 10:51:10 -0400

    sign up create user backend linked

--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -2,35 +2,6 @@ import React from 'react';
 
 
 class NewBaby extends React.Component {
-    render() {
-        return (
-            <div className='jumbotron container'>
-                    <h1> Sign Up New User </h1>
-                    <p>Please fill out the following to create an account</p>
-                    <form>
-                        <div className="form-group">
-                            <label for='email'><b>Email</b></label>
-                            <input type='text' className='form-control' placeholder='Enter Email' name='email'></input>
-                        </div>
-
-                        <div className="form-group">
-                            <label for='email' ><b>Password</b></label>
-                            <input type='text' className='form-control' placeholder='Enter Password' name='password'></input>
-                        </div>
-
-                        <div className="form-group">
-                            <label for='email' ><b> Re-Enter Password</b></label>
-                            <input type='text' className='form-control' placeholder='Re-Enter Password' name='ReEnterPassword'></input>
-                        </div>
-
-
-                        <div className="clearfix">
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                            <button type="button" class="btn btn-danger">Cancel</button>
-                        </div>
-                    </form>
-            </div>    
-=======
     constructor(props) {
         super(props);
         this.state = {
