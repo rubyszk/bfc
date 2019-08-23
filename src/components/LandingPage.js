7db9bf5eca7d94fb12b526179e8ba6b8f33a7c7a
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-08-23 14:24:26 -0400

    ladning page reworked to conditially render login and register buttons

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,26 +4,28 @@ import { Redirect } from 'react-router-dom';
 import './LandingPage.css'
 
 class LandingPage extends Component {
-  renderRedirect = () => {
-    if (this.props.currentUser !== null) {
-      return <Redirect to='/user' />;
-    }
-  };
   render() {
     return (
       <div className='landing'>
-      {this.renderRedirect()}
         <div className="d-flex align-items-center">
           <div className='jumbotron container'>
             <div className='main d-flex justify-content-center'>
               <h1 class="display-1"> Rumble </h1>
             </div>
 
-            <div className='d-flex justify-content-around'>
-              <Link to='/babies/duel'><button type="button" class="btn btn-primary">Enter</button></Link>
-              <Link to='/log-in'><button type="button" class="btn btn-primary">Log In</button></Link>
-              <Link to='/new-user'><button type="button" class="btn btn-primary">Register</button></Link>
-            </div>
+              {
+                this.props.currentUser !== null ?
+                <div className='d-flex justify-content-around'>
+                    <Link to='/babies/duel'><button type="button" class="btn btn-primary">Enter</button></Link>
+                </div>
+              :
+                <div className='d-flex justify-content-around'>
+                  <Link to='/babies/duel'><button type="button" class="btn btn-primary">Enter</button></Link>
+                  <Link to='/log-in'><button type="button" class="btn btn-primary">Log In</button></Link>
+                  <Link to='/new-user'><button type="button" class="btn btn-primary">Register</button></Link>
+              </div>
+            }
+
           </div>
         </div>
       </div>  
