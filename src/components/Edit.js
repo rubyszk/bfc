705ab7b48957191a616244a92b7ec581032868d9
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-26 10:19:01 -0400

    changed redirect routes and button color on user page

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 import { Form, Row, Col, Button } from 'react-bootstrap';
-import {Redirect} from 'react-router-dom';
-import NavigationBar from './NavigationBar.js'
+import { Redirect } from 'react-router-dom';
+import NavigationBar from './NavigationBar.js';
 class EditForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       toShowPage: false
-    }
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
     this.setState({
       toShowPage: true
-    })
+    });
     this.props.updateBaby();
   }
 
@@ -29,7 +29,7 @@ class EditForm extends React.Component {
   render() {
     return (
       <div>
-      {this.renderRedirect()}
+        {this.renderRedirect()}
         <form onSubmit={this.handleSubmit}>
           <div className='jumbotron container'>
             <h3>Edit {this.props.currentBaby.name} </h3>
@@ -153,7 +153,6 @@ class EditForm extends React.Component {
             </Form>
           </div>
         </form>
-      
       </div>
     );
   }
