f366154e336461d1b15a6c50a83c45882831ad1d
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-26 09:58:25 -0400

    commiting so that I can merge

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
-import './LandingPage.css'
-import ScrollableAnchor from 'react-scrollable-anchor'
+import './LandingPage.css';
 
 class LandingPage extends Component {
   render() {
     return (
       <div className='landing'>
-        <div class="header">
-              <h1 class="head">Let's get ready to...</h1>
-              <h1 class="logo"> Rumble </h1>
+        <div class='header'>
+          <h1 class='head'>Let's get ready to...</h1>
+          <h1 class='logo'> Rumble </h1>
         </div>
-              {
-                this.props.currentUser !== null ?
-                <div className='d-flex justify-content-around'>
-                    <Link to='/babies/duel'><button type="button" class="btn btn-secondary btn-lg btn-block"> Enter </button></Link>
-                </div>
-              :
-              <div id="links">
-                  <Link to='/babies/duel'><button type="button" id="btn1" class="btn btn-secondary btn-lg btn-block" >Enter</button></Link>
-              </div>
-             }
+        {this.props.currentUser !== null ? (
+          <div className='d-flex justify-content-around'>
+            <Link to='/babies/duel'>
+              <button type='button' class='btn btn-secondary btn-lg btn-block'>
+                {' '}
+                Enter{' '}
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div id='links'>
+            <Link to='/babies/duel'>
+              <button
+                type='button'
+                id='btn1'
+                class='btn btn-secondary btn-lg btn-block'
+              >
+                Enter
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
     );
   }
